ed9817a9d9228b8bab6c50800ffac1b9ffce0858
wlren <weilinwork99@gmail.com>
2024-03-28 21:47:16 +0800

    if else logic

--- main.py
+++ main.py
@@ -31,27 +31,48 @@ def main():
 
         column_data = {}
         primary_key = table["primary_key"]
-    
-        # # Composite key handling
-        if len(primary_key) > 1:
-            primary_key_data = gen.generate_composite_key_data(primary_key, table, seed)
-            for key in primary_key_data:
-                column_data[key] = primary_key_data[key]
+        foreign_keys = table["foreign_keys"]
 
-        
+        # Single key
         for column in table["columns"]:
-            if column["fieldName"] in primary_key and len(primary_key) > 1:
-                next
-            elif column["fieldName"] in primary_key:
-                column_data[column["fieldName"]] = gen.generate_primary_key_data(column, table, seed)
+            column_name = column["fieldName"]
+            if column_name in column_data:
+                continue
+            #Composite PK
+            if column_name in primary_key and len(primary_key) > 1:
+                composite_pk = handle_composite_pk_data(column, table, seed, primary_key)
+            #Composite FK
+            elif gen.is_foreign_key(table, column_name)[0] and gen.is_foreign_key(table, column_name)[1] > 1:
+                composite_fk = handle_composite_fk_data(column, table, seed, foreign_keys)
+            #Single PK (+ FK if total participation)
+            elif column_name in primary_key:
+                column_data[column_name] = gen.generate_primary_key_data(column, table, seed)
+            #Single FK
+            elif gen.is_foreign_key(table, column_name)[0]:
+                column_data[column_name] = gen.generate_foreign_key_data(column, table, seed)
+            #Speical type
             elif 'specialType' in column:
-                column_data[column["fieldName"]] = gen.generate_special_data(column, table, seed)
+                column_data[column_name] = gen.generate_special_data(column, table, seed)
+            #Normal type
             else:
-                column_data[column["fieldName"]] = gen.generate_column_data(column, table, seed)
+                column_data[column_name] = gen.generate_column_data(column, table, seed)
 
         df = pd.DataFrame(column_data)
 
         IOHandler.writeCSV(output_directory_path, df, table_name, exclude_header)
-    
+  
+  
+def handle_composite_pk_data(column, table, seed, primary_key):
+    # Check if pk is part of fk, if so, generate entire fk and return entire fk
+    primary_key_data = gen.generate_composite_key_data(primary_key, table, seed)
+    # if pk not in any fk, generate pk data
+    return primary_key_data
+
+def handle_composite_fk_data(column, table, seed, foreign_keys):
+    # just generate fk data
+    foreign_key_data = gen.generate_composite_key_data(foreign_keys, table, seed)
+
+    return foreign_key_data
+
 if __name__ == '__main__':
     main()
